jackadam1981/ChromeEc

rambi: Add acceleration data to LPC shared memory
After each read of the accelerometers, add accelerometer
data to LPC shared memory.

BUG=chrome-os-partner:25599
BRANCH=rambi
TEST=Manual test by reading the LPC shared memory through cros_ec
in the kernel.

Change-Id: If66df3fcb32b5423f4fa7dd471c219a1c4df7095
Signed-off-by: Alec Berg <alecaberg@chromium.org>

include/ec_commands.h
@@ -82,6 +82,12 @@
 #define EC_MEMMAP_BATT_SERIAL      0x70 /* Battery Serial Number String */
 #define EC_MEMMAP_BATT_TYPE        0x78 /* Battery Type String */
 #define EC_MEMMAP_ALS              0x80 /* ALS readings in lux (uint16_t) */
+#define EC_MEMMAP_ACC_STATUS       0x90 /* Accelerometer status */
+#define EC_MEMMAP_ACC_DATA         0x91 /* Accelerometer data 0x91 - 0xa4 */
+
+/* Define the format of the accelerometer mapped memory status byte. */
+#define EC_MEMMAP_ACC_STATUS_SAMPLE_ID_MASK  0x0f
+#define EC_MEMMAP_ACC_STATUS_BUSY_BIT        (1 << 4)
 
 /* Number of temp sensors at EC_MEMMAP_TEMP_SENSOR */
 #define EC_TEMP_SENSOR_ENTRIES     16

DHowett/ectool

BUG=none
Original-BUG=chrome-os-partner:25599
Original-Change-Id: If66df3fcb32b5423f4fa7dd471c219a1c4df7095
Reviewed-on: https://chromium-review.googlesource.com/186456
Reviewed-by: Randall Spangler <rspangler@chromium.org>
(cherry picked from commit 919ea7fe1f830235ae56829a8ee4435679dec124)
Change-Id: I10525c45e868d0b04aa84c27cab3b6baeda2b0d5
Reviewed-on: https://chromium-review.googlesource.com/187435

include/ec_commands.h
@@ -82,6 +82,12 @@
 #define EC_MEMMAP_BATT_SERIAL      0x70 /* Battery Serial Number String */
 #define EC_MEMMAP_BATT_TYPE        0x78 /* Battery Type String */
 #define EC_MEMMAP_ALS              0x80 /* ALS readings in lux (uint16_t) */
+#define EC_MEMMAP_ACC_STATUS       0x90 /* Accelerometer status */
+#define EC_MEMMAP_ACC_DATA         0x92 /* Accelerometer data 0x92 - 0xa5 */
+
+/* Define the format of the accelerometer mapped memory status byte. */
+#define EC_MEMMAP_ACC_STATUS_SAMPLE_ID_MASK  0x0f
+#define EC_MEMMAP_ACC_STATUS_BUSY_BIT        (1 << 4)
 
 /* Number of temp sensors at EC_MEMMAP_TEMP_SENSOR */
 #define EC_TEMP_SENSOR_ENTRIES     16