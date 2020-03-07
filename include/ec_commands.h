DHowett/ectool

commit 129a4f8753c29c6a372df83e3970e2e69d877cfc

common: Add feature flag for online calibration
This change adds a feature flag for online calibration.

BUG=b:138303429,chromium:1023858
BRANCH=None
TEST=buildall since flag is not yet used
Signed-off-by: Yuval Peress <peress@chromium.org>

Change-Id: I3036371a7499bf0eaf0846ff07eec6e6ee18a391
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/1834021
Reviewed-by: Jack Rosenthal <jrosenth@chromium.org>
Commit-Queue: Yuval Peress <peress@chromium.org>
Tested-by: Yuval Peress <peress@chromium.org>

include/ec_commands.h
@@ -2775,6 +2775,11 @@ struct ec_params_motion_sense {
 	};
 } __ec_todo_packed;
 
+enum motion_sense_cmd_info_flags {
+	/* The sensor supports online calibration */
+	MOTION_SENSE_CMD_INFO_FLAG_ONLINE_CALIB = BIT(0),
+};
+
 struct ec_response_motion_sense {
 	union {
 		/* Used for MOTIONSENSE_CMD_DUMP */
@@ -2824,6 +2829,33 @@ struct ec_response_motion_sense {
 			/* Max number of sensor events that could be in fifo */
 			uint32_t fifo_max_event_count;
 		} info_3;
+
+		/* Used for MOTIONSENSE_CMD_INFO version 4 */
+		struct __ec_align4 {
+			/* Should be element of enum motionsensor_type. */
+			uint8_t type;
+
+			/* Should be element of enum motionsensor_location. */
+			uint8_t location;
+
+			/* Should be element of enum motionsensor_chip. */
+			uint8_t chip;
+
+			/* Minimum sensor sampling frequency */
+			uint32_t min_frequency;
+
+			/* Maximum sensor sampling frequency */
+			uint32_t max_frequency;
+
+			/* Max number of sensor events that could be in fifo */
+			uint32_t fifo_max_event_count;
+
+			/*
+			 * Should be elements of
+			 * enum motion_sense_cmd_info_flags
+			 */
+			uint32_t flags;
+		} info_4;
 
 		/* Used for MOTIONSENSE_CMD_DATA */
 		struct ec_response_motion_sensor_data data;

commit 6d5f718b92e3e115334c764703280b445b50b0f2

ec_commands.h: add info_4 in sensor request structure
Fixes: 129a4f875 ("common: Add feature flag for online calibration")

BUG=chromium:1023858
TEST=compile
BRANCH=none

Signed-off-by: Gwendal Grignou <gwendal@chromium.org>
Change-Id: I7e7784e509062f28c8dc12d52fe8daed2ab73b23
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/ec/+/2084398
Reviewed-by: Daisuke Nojiri <dnojiri@chromium.org>

include/ec_commands.h
@@ -2715,7 +2715,7 @@ struct ec_params_motion_sense {
 		 */
 		struct __ec_todo_unpacked {
 			uint8_t sensor_num;
-		} info, info_3, data, fifo_flush, list_activities;
+		} info, info_3, info_4, data, fifo_flush, list_activities;
 
 		/*
 		 * Used for MOTIONSENSE_CMD_PERFORM_CALIB: